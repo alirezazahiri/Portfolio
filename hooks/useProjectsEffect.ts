deb960db7749dbaf01526d96a990d14a2f21d416
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-03-10 13:03:54 +0330

    useProjectsEffect Updated!

--- hooks/useProjectsEffect.ts
+++ hooks/useProjectsEffect.ts
@@ -4,43 +4,27 @@ import { useEffect, useState } from "react";
 import { getRepoDetails } from "services/repo-request";
 
 const useProjectsEffect = (items: ProjectItem[]) => {
-  const [items_, setItems_] = useState<
-    (ProjectItem & RepoDetailsType)[]
-  >([]);
+  const [items_, setItems_] = useState<(ProjectItem & RepoDetailsType)[]>([]);
 
   useEffect(() => {
-    const setDetails = async () => {
+    (async () => {
+      /**
+       * @returns the mapped list with the data given from API
+       */
       const promises = items.map(async (item) => {
-        const newItem: ProjectItem & RepoDetailsType = {
-          ...item,
-          stars: 0,
-          topics: [],
-          homepage: "",
-          description: ""
-        };
-
-        const getDetails = async () => {
+        return (async () => {
           const { data } = await getRepoDetails(item.repoName);
-          newItem.stars = data.stars;
-          newItem.topics = data.topics;
-          newItem.homepage = data.homepage;
-          newItem.description = data.description;
-
-          return newItem;
-        };
-
-        return getDetails();
+          return { ...data, ...item };
+        })(); // map the data
       });
 
       const result = await Promise.all(promises);
 
       setItems_(result);
-    };
-
-    setDetails();
+    })(); // set the data to state
   }, []);
 
-  return [items_];
+  return [items_]; // return the new data
 };
 
 export default useProjectsEffect;
