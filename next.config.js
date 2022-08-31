43e92a75ae822f05018b2e2d5d5d985cb8c88b44
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-08-31 10:52:26 +0430

    .env.local variables are set!

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ module.exports = {
     domains: ["media.graphassets.com"],
   },
   env: {
-    NEXT_APP_HYGRAPH_URI:
-      "https://api-us-west-2.hygraph.com/v2/cl64zbv668lze01t306uq3fb9/master",
-    NEXT_APP_PROFILE_ID: "cl66aj52prtna0cn7w27wma2e",
-    NEXT_APP_ABOUT_ID: "cl66c2menwvy60bn72bsijbme",
-    NEXT_APP_CV_ID: "cl66qtuxj69n10bn79wr8u0pj",
+    NEXT_APP_HYGRAPH_URI: process.env.NEXT_APP_HYGRAPH_URI,
+    NEXT_APP_PROFILE_ID: process.env.NEXT_APP_PROFILE_ID,
+    NEXT_APP_ABOUT_ID: process.env.NEXT_APP_ABOUT_ID,
+    NEXT_APP_CV_ID: process.env.NEXT_APP_CV_ID,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   },
   experimental: {
     styledComponents: true,
