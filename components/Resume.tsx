b54cd1ff6a47f2b9f2dbc1aec09bc13be82dd102
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-09-05 23:26:04 +0430

    initial hard coded resume created!

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,11 +1,151 @@
 import React from "react";
-import ResumeLayout from "./common/ResumeLayout";
+import { Grid, Typography } from "@mui/material";
+import { Badges, Caption, Title, UnorderedList } from "./common/Resume.Common";
+
+// icons
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import TerminalIcon from "@mui/icons-material/Terminal";
+
+const TEXT =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, iste nobis magnam sequi aliquid labore alias excepturi impedit harum cumque, necessitatibus, id aut blanditiis libero quibusdam dolores! Facere, nemo quasi.";
 
 const Resume = () => {
   return (
-    <>
-      
-    </>
+    <Grid container spacing={2} px={1} py={2}>
+      <Grid item xs={6}>
+        <Grid item>
+          <Title size="lg" text="Story Book" />
+          <Caption color="var(--dark-grey-caption)" text={TEXT} />
+        </Grid>
+        <Grid item>
+          <Title size="lg" text="Recent Experiences" />
+          <Title size="md" pt={2} text="React Developer" />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--dark-grey-caption)"
+            text={"mei li bo si - full time"}
+          />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--prog-bar-light-blue)"
+            text={"Jul 2020 - Sep 2020"}
+          />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--grayish-blue)"
+            text={"Shanghai, China"}
+          />
+          <Caption pt={6 / 8} color="var(--dark-grey-caption)" text={TEXT} />
+        </Grid>
+        <Grid item>
+          <Title size="lg" text="Education" />
+          <Title size="md" pt={2} text="Sharif University of Technology" />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--dark-grey-caption)"
+            text={"Bachelor's degree - Material Science and Engineering"}
+          />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--prog-bar-light-blue)"
+            text={"2019 - 2020"}
+          />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--grayish-blue)"
+            text={"Tehran, Iran"}
+          />
+          <Caption pt={6 / 8} color="var(--dark-grey-caption)" text={TEXT} />
+          <Title size="md" pt={2} text="Babol Noshirvani University of Technology" />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--dark-grey-caption)"
+            text={"Bachelor's degree - Computer Software Engineering"}
+          />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--prog-bar-light-blue)"
+            text={"2020 - 2023"}
+          />
+          <Caption
+            pt={2 / 8}
+            pb={0}
+            color="var(--grayish-blue)"
+            text={"Babol, Iran"}
+          />
+          <Caption pt={6 / 8} color="var(--dark-grey-caption)" text={TEXT} />
+        </Grid>
+      </Grid>
+      <Grid item xs={6}>
+        <Grid item>
+          <Title size="lg" text="Technical Skills" mention="(in order)" />
+          <Title size="md" pt={2} text="Software Engineering" />
+          <Caption pt={1} color="var(--dark-grey-caption)" text={TEXT} />
+          <Title size="md" text="Client" mention="(proficient)" />
+          <Caption pt={1} color="var(--dark-grey-caption)" text={TEXT} />
+          <Title size="md" text="Industry" />
+          <Caption pt={1} color="var(--dark-grey-caption)" text={TEXT} />
+          <Title size="md" text="Server" mention="(familiar)" />
+          <Caption pt={1} color="var(--dark-grey-caption)" text={TEXT} />
+        </Grid>
+        <Grid item>
+          <Title size="lg" text="Soft Skills" />
+          <Badges
+            badges={[
+              "problem-solving",
+              "self-motivated",
+              "responsibility",
+              "confidence",
+              "communication",
+              "hard-working",
+              "teamwork",
+              "passionate to learn",
+            ]}
+          />
+        </Grid>
+        <Grid item>
+          <Title size="lg" text="Books and Courses" />
+          <UnorderedList
+            items={[
+              "sdkjfhksdgbkfdhgisdf sdkfjd , skfdh k",
+              "sdjfj , ksdf kjhksd , sdkf k",
+              "skjbfjdksf",
+            ]}
+          />
+        </Grid>
+        <Grid item>
+          <Title size="lg" text="Interests" />
+          <Grid container spacing={2} px={0} py={0}>
+            <Grid item display="flex" alignItems="center" xs={6}>
+              <YouTubeIcon fontSize="large" sx={{ pr: 1 }} />
+              <Caption text="Youtube" color="var(--dark-grey-caption)" />
+            </Grid>
+            <Grid item display="flex" alignItems="center" xs={6}>
+              <MusicNoteIcon fontSize="large" sx={{ pr: 1 }} />
+              <Caption text="Music" color="var(--dark-grey-caption)" />
+            </Grid>
+            <Grid item display="flex" alignItems="center" xs={6}>
+              <SportsEsportsIcon fontSize="large" sx={{ pr: 1 }} />
+              <Caption text="Games" color="var(--dark-grey-caption)" />
+            </Grid>
+            <Grid item display="flex" alignItems="center" xs={6}>
+              <TerminalIcon fontSize="large" sx={{ pr: 1 }} />
+              <Caption text="Coding" color="var(--dark-grey-caption)" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
---
+++ layout/ResumeLayout.tsx
@@ -0,0 +1,186 @@
+import React, { FC } from "react";
+
+// mui
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Container,
+  Avatar,
+  Grid,
+  createTheme, ThemeProvider
+} from "@mui/material";
+
+// icons
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PhoneInTalkOutlinedIcon from "@mui/icons-material/PhoneInTalkOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Encode Sans SC, sans-serif",
+  },
+});
+
+const Header: FC = () => {
+  return (
+    <AppBar
+      sx={{
+        bgcolor: "var(--midnight-blue)",
+        height: 200,
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        borderBottom: "1px solid var(--prog-bar-light-blue)",
+      }}
+      position="relative"
+    >
+      <Container
+        maxWidth="md"
+        sx={{ minHeight: "fit-content" }}
+        className="normalize"
+      >
+        <Toolbar>
+          <Grid container alignItems="center">
+            <Avatar
+              alt="Alireza Zahiri"
+              sx={{ mr: 1, width: 128, height: 128 }}
+            />
+            <Grid item xs={6} sm container ml={2}>
+              <Grid item xs container direction="column" spacing={1}>
+                <Typography
+                  fontWeight={900}
+                  fontSize="18px"
+                  letterSpacing="2px"
+                  ml="-4px"
+                  lineHeight={2}
+                >
+                  Alireza Zahiri
+                </Typography>
+                <Typography
+                  fontWeight={400}
+                  lineHeight={2}
+                  color="var(--light-grey)"
+                >
+                  Software Engineer
+                </Typography>
+                <Typography
+                  fontWeight={400}
+                  lineHeight={2}
+                  ml="-4px"
+                  color="var(--light-grey)"
+                >
+                  21 years old
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={1}>
+                <Grid
+                  item
+                  display="flex"
+                  justifyContent="flex-end"
+                  alignItems="center"
+                >
+                  <Typography fontWeight={300} lineHeight={2} fontSize="14px">
+                    <a href="mailto:itsalireza.zr@gmail.com">
+                      itsalireza.zr@gmail.com
+                    </a>
+                  </Typography>
+                  <EmailOutlinedIcon
+                    sx={{ color: "var(--light-grey-2)", ml: 1 }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  display="flex"
+                  justifyContent="flex-end"
+                  alignItems="center"
+                >
+                  <Typography fontWeight={300} lineHeight={2} fontSize="14px">
+                    +98 911 641 7260
+                  </Typography>
+                  <PhoneInTalkOutlinedIcon
+                    sx={{ color: "var(--light-grey-2)", ml: 1 }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  display="flex"
+                  justifyContent="flex-end"
+                  alignItems="center"
+                >
+                  <Typography fontWeight={300} lineHeight={2} fontSize="14px">
+                    Mazandaran, Iran
+                  </Typography>
+                  <LocationOnOutlinedIcon
+                    sx={{ color: "var(--light-grey-2)", ml: 1 }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  display="flex"
+                  justifyContent="flex-end"
+                  alignItems="center"
+                >
+                  <Typography fontWeight={300} lineHeight={2} fontSize="14px">
+                    <a href="https://linkedin.com/in/alireza-zahiri">
+                      linkedin.com/alireza-zahiri
+                    </a>
+                  </Typography>
+                  <LinkedInIcon sx={{ color: "var(--light-grey-2)", ml: 1 }} />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <Typography
+        bgcolor="var(--midnight-blue)"
+        color="primary"
+        padding="10px"
+        textAlign="center"
+        fontSize="12px"
+        fontWeight={300}
+        sx={{
+          borderTop: "1px solid var(--prog-bar-light-blue)",
+        }}
+      >
+        Designed and Built by Alireza Zahiri - 2021
+      </Typography>
+    </footer>
+  );
+};
+
+interface Props {
+  children: JSX.Element;
+}
+
+const ResumeLayout: FC<Props> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container
+        maxWidth="md"
+        sx={{ bgcolor: "var(--background-alt)", height: "100vh" }}
+        className="normalize"
+        component="div"
+      >
+        <Header />
+        <main style={{ backgroundColor: "var(--light-grey-2)" }}>
+          {children}
+        </main>
+        <Footer />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default ResumeLayout;
