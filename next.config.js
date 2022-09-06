7831f811a501f507cf9e6d7c721af1fb8d721023
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-09-06 11:58:39 +0430

    next-configs updated

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = {
     NEXT_APP_ABOUT_ID: process.env.NEXT_APP_ABOUT_ID,
     NEXT_APP_CV_ID: process.env.NEXT_APP_CV_ID,
     NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+    NEXT_APP_CV_STORYBOOK_ID: process.env.NEXT_APP_CV_STORYBOOK_ID
   },
   experimental: {
     styledComponents: true,
