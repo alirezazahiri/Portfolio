67b63b455389d39361aadbcf41781ed610773742
Alireza Zahiri <itsalireza.zr@gmail.com>
2022-09-19 19:27:54 +0430

    sitemap created!

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,14 @@ const Page: NextPage = () => {
       <Head>
         <title>Alireza Zahiri</title>
         <meta name="description" content="Portfolio, Alireza Zahiri" />
-        <meta name="viewport" content="width=device-width"/>
+        <meta name="viewport" content="width=device-width" />
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
+        <link
+          rel="canonical"
+          href="https://github.com/alirezazahiri"
+          key="canonical"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Home isLoading={loading} profile={profile} error={error} />
---
+++ pages/sitemap.xml.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import * as fs from "fs";
+
+const Sitemap = () => {
+  return null;
+};
+
+export default Sitemap;
+
+export const getServerSideProps = async ({ res }) => {
+  const BASE_URL = window.location.protocol + "//" + window.location.host;
+
+  const staticPaths = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "_app.tsx",
+        "_document.tsx",
+        "404.tsx",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${BASE_URL}/${staticPagePath}`;
+    });
+
+  const allPaths = [...staticPaths];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${allPaths
+      .map((url) => {
+        return `
+          <url>
+            <loc>${url}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+          </url>
+        `;
+      })
+      .join("")}
+  </urlset>
+`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
