f7c28bddf288b4fc28803eab4331236670def57c
alirezazahiri <itsalireza.zr@gmail.com>
2025-12-26 14:13:45 +0330

    refactor: delete all the legacy code

--- src/lib/types/project.d.ts
+++
@@ -1,8 +0,0 @@
-export type Project = {
-	id: string;
-	title: string;
-	technologies: string[];
-	link?: string;
-	website: string;
-	opensource: boolean;
-};
--- src/lib/utils/date.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date: Date) => {
-	return date.toLocaleString('en-US', {
-		month: 'long',
-		day: 'numeric',
-		year: 'numeric'
-	});
-};
